432a421ac839d7ac096dd58dd5858a3d799ea6fe
J4qk L1nk0ln <t.kulik5@mail.ru>
2026-05-04 21:08:34 +0500

    update 04.05.2026

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ simData = jb.load("E:/big models/similarities.pkl")
 
 recDataFrame = pd.DataFrame(recData)
 
+def get_poster(name):
+    url = f"https://api.deezer.com/search?q={name}"
+
+    try:
+        res = requests.get(url)
+        data = res.json()
+
+        if 'data' in data and len(data['data']) > 0:
+            return data['data'][0]['album']['cover_big']
+    
+    except Exception as e:
+        print("api error: ", e)
+        return 0
 
 def recommend(name):
     music_row = recDataFrame[recDataFrame['name'].str.contains(name, case=False, na=False, regex=False)]
@@ -26,31 +39,53 @@ def recommend(name):
     music_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommendList = []
+    recommendPosterList = []
 
     for i in music_list:
         music_name = recDataFrame.iloc[i[0]]['name']
         recommendList.append(music_name)
+        recommendPosterList.append(get_poster(recDataFrame.iloc[i[0]]['name']))
+
     
-    return recommendList
+    return recommendList, recommendPosterList
 
 selected_music_name = st.selectbox("Select a music you like", recDataFrame["name"].values)
 
 
 
 if st.button("Recommend"):
-    names = recommend(selected_music_name)
-
+    names, posters = recommend(selected_music_name)
     col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
         st.text(names[0])
+        if posters[0]:
+            st.image(posters[0])
+        else:
+            st.write("There is no poster for this music")
     with col2:
         st.text(names[1])
+        if posters[1]:
+            st.image(posters[1])
+        else:
+            st.write("There is no poster for this music")
     with col3:
         st.text(names[2])
+        if posters[2]:
+            st.image(posters[2])
+        else:
+            st.write("There is no poster for this music")
     with col4:
         st.text(names[3])
+        if posters[3]:
+            st.image(posters[3])
+        else:
+            st.write("There is no poster for this music")
     with col5:
         st.text(names[4])
+        if posters[4]:
+            st.image(posters[4])
+        else:
+            st.write("There is no poster for this music")
     
     
